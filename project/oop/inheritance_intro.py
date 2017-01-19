fc5934a950ba91ce1d437518997f8087037e9411
pekkipo <pekkipodev@gmail.com>
2017-01-19 16:14:59 +0300

    Initial

---
+++ project/oop/inheritance_intro.py
@@ -0,0 +1,34 @@
+class Decimal:
+    def __init__(self, number, places):
+        self.number = number
+        self.places = places
+
+    def __repr__(self):
+        return "%.{}f".format(self.places) % self.number
+
+
+class Currency(Decimal):
+    # inherited from Decimal. Has all its props and methods
+
+    def __init__(self, symbol, number, places):
+        super().__init__(number, places)
+        # necessary to inlcude parent class props
+        # can play around with those values in super.init
+        # can do number - 1 for instance
+        self.symbol = symbol
+        # property of Currency specifically
+
+    def __repr__(self): # override repr method of Decimal
+        return "{}{}".format(self.symbol, super().__repr__())
+        # called a method of a parent Decimal class
+
+
+    def add_currency(self, currency):
+        # availabe for Currency class
+        # not available for Decimal class
+        pass
+
+
+
+
+print(Decimal(15.6789,3))
